e1c432ab4ef243358da02fc7db321f2fa508d500
CosmicAnon <cosmicanon1320@gmail.com>
2022-01-31 18:39:16 +0400

    content and media updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Route, Switch, Redirect } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import ghoulXLogo from "./assets/GhoulX logo.png";
-import ghoulLogo from "./assets/ghoul_logo.svg";
+import ghoulXLogo from "./assets/GhoulX_logo.png";
+import gdaiLogo from "./assets/gdai_logo.svg";
 import borrowLogo from "./assets/borrow.svg";
 import hodlLogo from "./assets/hodl.svg";
 import tradeLogo from "./assets/trade.svg";
 import spendGdaiLogo from "./assets/spendgdai.svg";
 import launchAppArrow from "./assets/launchAppArrow.svg";
-import twitterLogo from "./assets/twitter (2).svg";
-import telegramLogo from "./assets/telegram (2).svg";
+import twitterLogo from "./assets/twitter.svg";
+import telegramLogo from "./assets/telegram.svg";
 import "./App.css";
 
 function App() {
@@ -17,12 +17,13 @@ function App() {
       <Route path="/" exact>
         <Layout>
           <div className="header-container">
-            <img src={ghoulLogo} alt="ghoulLogo" id="header-logo" />
+            <img src={gdaiLogo} alt="gdaiLogo" id="header-logo" />
             <div className="header-elem">
               <div id="buy-ghoul">
                 <a
-                  href="https://pancakeswap.finance/swap?outputCurrency=0x171ce6141e7a5980222bc6b757ee2f1f95b3264e"
+                  href="https://pancakeswap.finance/swap?outputCurrency=0xcfc5cf2a73e97401c218b7ce1c594524bd8ba4bf"
                   target="_blank"
+                  rel="noreferrer"
                 >
                   {" "}
                   <div id="buy-ghoul-div">
@@ -36,7 +37,7 @@ function App() {
                   </div>
                 </a>
               </div>
-              <a href="https://app.ghoul.finance/" target="_blank">
+              <a href="https://app.ghoul.finance/" >
                 <div id="launch-app">Launch App</div>
               </a>
             </div>
@@ -44,23 +45,27 @@ function App() {
           <div className="horiz-line-mobile"></div>
           <div className="hero-one-container">
             <div className="hero-one-content">
-              <div className="title-1">GhostDai The</div>
+              <div className="title-1">GhostDAI The</div>
               <div className="title-2">
                 <span id="percent">0%</span> Interest Stablecoin
               </div>
               <div className="sub-title">
-                gDai is a stablecoin backed with collateral of your crypto
-                holding
+                gDAI is a stablecoin backed with collateral of your crypto
+                holdings
               </div>
               <div className="sub-title-buttons" target="_blank">
-                <a href="https://app.ghoul.finance/" target="_blank">
+                <a href="https://app.ghoul.finance/" >
                   <div id="learn-more">Launch App &#62;</div>
                 </a>
-                <a href="https://docs.ghoul.finance/" target="_blank">
+                <a href="https://docs.ghoul.finance/" target="_blank" rel="noreferrer">
                   <div id="launch-app-2">Learn More</div>
                 </a>
               </div>
               {/* <div className="volume-cards">
+                <div className="volume-card-elem">
+                  <div className="trading-volume">TOTAL VALUE LOCKED</div>
+                  <div className="dollar-value">$1,000,000</div>
+                </div>
                 <div className="volume-card-elem">
                   <div className="trading-volume">TOTAL TRADING VOLUME</div>
                   <div className="dollar-value">$1,000,000</div>
@@ -75,7 +80,7 @@ function App() {
           <div className="hero-two-container">
             <div className="horiz-line"></div>
             <div className="hero-two-subtitle">
-              No credit checks or approvals needed, 0% interest, pay back on
+              Unlock the value of your crypto assets while you HODL.<br/> No credit checks or approvals needed, 0% interest, pay back on
               your own timeline.
             </div>
             <div className="hero-two-cards">
@@ -85,38 +90,36 @@ function App() {
                 </div>
                 <div className="ht-card-title">Borrow</div>
                 <div className="ht-card-sub-title">
-                  Use your crypto to borrow up to 2/3 of their value in gDAI.
+                  Add your crypto assets to personal vaults and borrow up to 2/3 of their value in gDAI.
                 </div>
-                <div className="ht-card-button">Buy gDai</div>
-              </div>
-              <div className="ht-card-elem">
-                <img src={hodlLogo} alt="" />
-                <div className="ht-card-title">Hodl</div>
-                <div className="ht-card-sub-title">
-                  No credit checks or approvals needed, 0% interest, pay back on
-                  your own timeline.
-                </div>
-                <div className="ht-card-button">Borrow</div>
+                <a href="https://app.ghoul.finance/#/vaults" target="_blank" rel="noreferrer"><div className="ht-card-button">Create Vault</div></a>
               </div>
               <div className="ht-card-elem">
                 <img src={tradeLogo} alt="" />
                 <div className="ht-card-title">Trade</div>
                 <div className="ht-card-sub-title">
-                  Earn GhoulX governance tokens by participating in our
-                  liquidity mining programs
+                  Spend your gDAI, swap them for other stablecoins or trade them for other tokens.
                 </div>
-                <div className="ht-card-button">Add Liquidity</div>
+                <a href="https://app.ghoul.finance/#/swap" target="_blank" rel="noreferrer"><div className="ht-card-button">Swap Tokens</div></a>
+              </div>
+              <div className="ht-card-elem">
+                <img src={hodlLogo} alt="" />
+                <div className="ht-card-title">Farm</div>
+                <div className="ht-card-sub-title">
+                  Earn GhoulX governance tokens by participating in our liquidity mining programs.
+                </div>
+                <a href="https://app.ghoul.finance/#/farm" target="_blank" rel="noreferrer"><div className="ht-card-button">Add Liquidity</div></a>
               </div>
               <div className="ht-card-elem">
                 <img src={spendGdaiLogo} alt="" />
-                <div className="ht-card-title">Spend gDai</div>
+                <div className="ht-card-title">Participate</div>
                 <div className="ht-card-sub-title">
-                  Be part of a community that helps govern the future of GhoulX
-                  Dao
+                  Be part of a GhoulDAO community that helps improve and govern the future of the protocol.
                 </div>
-                <div className="ht-card-button">Earn GhoulX</div>
+                <a href="https://snapshot.org/#/ghouldao.eth" target="_blank" rel="noreferrer"><div className="ht-card-button">Govern gDAI</div></a>
               </div>
             </div>
+            <a href="https://app.ghoul.finance/" >
             <div className="launch-app">
               Launch App
               <span id="launch-app-arrow">
@@ -124,103 +127,31 @@ function App() {
                 <img src={launchAppArrow} alt="" />
               </span>
             </div>
+            </a>
+            <br/><br/>
+            <a href="https://docs.ghoul.finance/" target="_blank" rel="noreferrer">
+            <div className="launch-app-2">
+              View Documentation
+            </div>
+            </a>
+
           </div>
           <div className="footer-container">
             <div className="footer-col-1">
               <div className="row-one-c-1">
-                <img src={ghoulLogo} alt="" />
+                <img src={gdaiLogo} alt="" />
                 <div className="by-p-labs">By Poltergiest Labs</div>
               </div>
               <div className="row-two-c-1">
-                <a href="https://t.me/poltergeistlabs" target="_blank">
+                <a href="https://t.me/poltergeistlabs" target="_blank" rel="noreferrer">
                   <img src={telegramLogo} alt="" />
                 </a>
-                <a href="https://twitter.com/Poltergeistlabs" target="_blank">
+                <a href="https://twitter.com/Poltergeistlabs" target="_blank" rel="noreferrer">
                   <img src={twitterLogo} alt="" />
                 </a>
               </div>
             </div>
-            <div className="footer-col-2">
-              <div className="row-one-c-2">
-                <div className="col-title">Getting Started</div>
-                <a
-                  href="https://docs.ghoul.finance/tokenomics/ghoulx-token"
-                  className="col-content"
-                  target="_blank"
-                >
-                  Tokenomics
-                </a>
-                <a
-                  href="https://docs.ghoul.finance/introduction/general"
-                  className="col-content"
-                  target="_blank"
-                >
-                  Ecosystem
-                </a>
-                <a href="" className="col-content" target="_blank">
-                  Roadmap
-                </a>
-                <a
-                  href="https://docs.ghoul.finance/"
-                  className="col-content"
-                  target="_blank"
-                >
-                  gDai Documentation
-                </a>
-                <a
-                  href="https://gateway.btfs.io/btfs/QmbxSZCGygHzYgDyaUAYpBKxZAHnz3JgBka5HUcyhk6JXc"
-                  className="col-content"
-                  target="_blank"
-                >
-                  Original Darkpaper
-                </a>
-                <a
-                  href="https://www.redbubble.com/people/poltergeistlabs/shop?asc=u"
-                  className="col-content"
-                  target="_blank"
-                >
-                  Shop
-                </a>
-              </div>
-              <div className="row-two-c-2">
-                <div className="col-title">Getting Started</div>
-                <a
-                  href="https://poltergeistlabs.medium.com/how-to-bridge-ghoul-tokens-from-eth-to-bsc-66bd6e11ba59"
-                  className="col-content"
-                  target="_blank"
-                >
-                  Ghoul ETH to BSC Bridge
-                </a>
-                <a
-                  href="https://pancakeswap.finance/swap?outputCurrency=0x171ce6141e7a5980222bc6b757ee2f1f95b3264e"
-                  className="col-content"
-                  target="_blank"
-                >
-                  Ghoul on Pancakeswap
-                </a>
-                <a
-                  href="https://bscscan.com/token/0x171ce6141e7a5980222bc6b757ee2f1f95b3264e"
-                  className="col-content"
-                  target="_blank"
-                >
-                  Ghoul BSC Contract
-                </a>
-                <a
-                  href="https://etherscan.io/token/0x3bb86d867a9f3addf994cdadb210fa82f0d4157a"
-                  className="col-content"
-                  target="_blank"
-                >
-                  Ghoul ETH Contract
-                </a>
-                <a
-                  href="https://www.coingecko.com/en/coins/ghoul-token"
-                  className="col-content"
-                  target="_blank"
-                >
-                  Ghoul on Coingecko
-                </a>
-              </div>
-            </div>
+
           </div>
         </Layout>
         <Redirect from="*" to="/" />
